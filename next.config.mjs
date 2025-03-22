206eb79f05bed53926cf60d34d5dc0055725708f
Pankaj Kumar <raipankaj150@gmail.com>
2025-03-22 16:17:16 +0530

    config fix

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,20 @@ const nextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "i.ytimg.com",
+        pathname: "/vi/**", // Optional: for YouTube thumbnails
+      },
+      {
+        protocol: "https",
+        hostname: "yt3.ggpht.com",
+        pathname: "**", // Allows all paths under yt3.ggpht.com
+      },
+    ],
+  },
 };
 
 export default nextConfig;
